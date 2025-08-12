100ad1b11a110fde8adcc1106b1f5a9442cdc698
dotae1 <9636515@gmail.com>
2025-08-12 13:05:07 +0900

    초기세팅

---
+++ src/main/java/com/icando/ICanDoApplication.java
@@ -0,0 +1,13 @@
+package com.icando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ICanDoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ICanDoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/icando/ICanDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ICanDoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
