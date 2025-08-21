2525c899a8fed58dca534e186a356ac719751c22
한상희 <tkdgml822@gmail.com>
2025-08-22 01:47:53 +0900

    feat: 연관관계 및 레포지스토리 구현

--- src/main/java/com/icando/writing/repository/TopicRepository.java
+++ src/main/java/com/icando/writing/repository/TopicRepository.java
@@ -1,7 +1,20 @@
 package com.icando.writing.repository;
 
 import com.icando.writing.entity.Topic;
+import com.icando.writing.enums.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface TopicRepository extends JpaRepository<Topic, Long> {
+    List<Topic> findByCategory(Category category);
+
+    @Query(value = "SELECT * FROM topic t WHERE t.category = :category ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
+    Optional<Topic> findRandomByCategory(@Param("category") String category);
+
+    @Query(value = "SELECT * FROM topic ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
+    Optional<Topic> findRandom();
 }
