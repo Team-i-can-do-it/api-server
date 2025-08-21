1898438f6639720be4e63dcd735377558fb2514d
paka <wjd1357912@gmail.com>
2025-08-22 01:37:53 +0900

    fix : 누락 어노테이션 추가

--- src/main/java/com/icando/ICanDoApplication.java
+++ src/main/java/com/icando/ICanDoApplication.java
@@ -2,8 +2,10 @@ package com.icando;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ICanDoApplication {
 
     public static void main(String[] args) {
