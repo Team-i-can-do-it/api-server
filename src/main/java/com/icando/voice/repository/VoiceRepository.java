76dedbcae130a88aecd8b7a368babc55e0c377b3
dotae1 <9636515@gmail.com>
2025-08-21 14:18:11 +0900

    기획 변경으로 인한 도메인 삭제 & 설정파일 수정

--- src/main/java/com/icando/voice/repository/VoiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.icando.voice.repository;
-
-import com.icando.voice.entity.Voice;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VoiceRepository extends JpaRepository<Voice, Long> {
-}
