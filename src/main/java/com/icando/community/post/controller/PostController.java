f825ac74ecad327436c9155079e3bc2f73681e43
paka <wjd1357912@gmail.com>
2025-08-22 01:39:18 +0900

    feat : 게시글 작성 API 구현

--- src/main/java/com/icando/community/post/controller/PostController.java
+++ src/main/java/com/icando/community/post/controller/PostController.java
@@ -1,11 +1,29 @@
 package com.icando.community.post.controller;
 
+import com.icando.community.post.dto.PostCreateRequest;
+import com.icando.community.post.exception.PostSuccessCode;
+import com.icando.community.post.service.PostService;
+import com.icando.global.success.SuccessResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/post")
 @RequiredArgsConstructor
 public class PostController {
+
+    private final PostService postService;
+
+    //TODO: 추후 auth 관련 로직 개발 뒤 @AuthenticationPrincipal 사용 예정
+    @PostMapping
+    public ResponseEntity<SuccessResponse> createPost(
+            @RequestBody @Valid PostCreateRequest postCreateRequest,
+            @RequestParam Long memberId) {
+
+        postService.createPost(postCreateRequest,memberId);
+        return ResponseEntity.ok(
+                SuccessResponse.of(PostSuccessCode.SUCCESS_CREATE_POST));
+    }
 }
