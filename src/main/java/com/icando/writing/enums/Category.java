bc405d39c4bde198fa7c9701b33313588e75c942
한상희 <tkdgml822@gmail.com>
2025-08-22 01:47:12 +0900

    feat: enums 및 response 정의

--- src/main/java/com/icando/writing/enums/Category.java
+++ src/main/java/com/icando/writing/enums/Category.java
@@ -3,16 +3,26 @@ package com.icando.writing.enums;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Arrays;
+
 @RequiredArgsConstructor
 @Getter
 public enum Category {
-    DAILY_LIFE("일상취미"),
-    SOCIAL_POLITICS("사회정치"),
-    ECONOMY_BUSINESS("경제비즈니스"),
-    TECH_FUTURE("기술미래"),
-    CULTURE_ARTS("문화예술"),
-    RANDOM("랜덤");
+    
+    DAILY_LIFE("daily-life", "일상취미"),
+    SOCIAL_POLITICS("social-politics", "사회정치"),
+    ECONOMY_BUSINESS("economy-business", "경제비즈니스"),
+    TECH_FUTURE("tech-future" , "기술미래"),
+    CULTURE_ARTS("culture-arts" , "문화예술"),
+    RANDOM("random", "랜덤");
 
-    private final String topic;
+    private final String path;
+    private final String description;
 
+    public static Category fromPath(String path) {
+        return Arrays.stream(Category.values())
+            .filter(category -> category.getPath().equalsIgnoreCase(path))
+            .findFirst()
+            .orElse(null);
+    }
 }
