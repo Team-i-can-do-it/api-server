7a700ee093125c89290ed7f698f300f122183875
한상희 <tkdgml822@gmail.com>
2025-08-21 18:05:53 +0900

    feat: 주제 엔티티 추가

---
+++ src/main/java/com/icando/writing/entity/Topic.java
@@ -0,0 +1,23 @@
+package com.icando.writing.entity;
+
+
+import com.icando.writing.enums.Category;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Topic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "topic_id", nullable = false)
+    private Long Id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "category")
+    private Category category;
+
+    @Column(name = "topic")
+    private String topic;
+}
