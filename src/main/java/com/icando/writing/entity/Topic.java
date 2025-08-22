dfc832594031363362a9f76e1f52cec734d69e8a
한상희 <tkdgml822@gmail.com>
2025-08-22 10:31:08 +0900

    hotfix: Topic JPA 기본 생성자 오류 해결

--- src/main/java/com/icando/writing/entity/Topic.java
+++ src/main/java/com/icando/writing/entity/Topic.java
@@ -3,10 +3,13 @@ package com.icando.writing.entity;
 
 import com.icando.writing.enums.Category;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Topic {
 
     @Id
