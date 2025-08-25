8cf3683184f2b56d8ec3db641323256ba3c996aa
Huey S. Jeong <wjdgnltkd123@gmail.com>
2025-08-25 16:07:38 +0900

    feat: 문단 완성 에러 및 성공 코드 추가

--- src/main/java/com/icando/paragraphCompletion/exception/ParagraphCompletionErrorCode.java
+++ src/main/java/com/icando/paragraphCompletion/exception/ParagraphCompletionErrorCode.java
@@ -10,7 +10,8 @@ public enum ParagraphCompletionErrorCode implements ErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
     WORD_NOT_IN_CONTENT(HttpStatus.BAD_REQUEST, "내용에 포함되어 있지 않은 단어가 있습니다."),
     INVALID_WORD_COUNT(HttpStatus.BAD_REQUEST, "단어 목록이 비어있습니다."),
-    INVALID_CONTENT(HttpStatus.BAD_REQUEST, "내용이 비어있습니다."),;
+    INVALID_CONTENT(HttpStatus.BAD_REQUEST, "내용이 비어있습니다."),
+    PARAGRAPH_COMPLETION_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 문단완성 글이 존재하지 않습니다.");
 
     private final HttpStatus status;
     private final String message;
