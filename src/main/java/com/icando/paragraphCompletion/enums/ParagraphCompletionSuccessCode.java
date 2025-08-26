aeb0a51dd3dccebec4848f6532ddfd5be94503a5
Huey S. Jeong <wjdgnltkd123@gmail.com>
2025-08-26 10:44:59 +0900

    feat: 문단 완성 글 전체 조회 성공 코드 추가

--- src/main/java/com/icando/paragraphCompletion/enums/ParagraphCompletionSuccessCode.java
+++ src/main/java/com/icando/paragraphCompletion/enums/ParagraphCompletionSuccessCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 public enum ParagraphCompletionSuccessCode implements SuccessCode {
     RANDOM_WORD_SUCCESS(HttpStatus.OK, "랜덤 단어 조회 성공"),
     PARAGRAPH_COMPLETION_CREATE_SUCCESS(HttpStatus.CREATED, "문단 완성 글 작성 성공"),
-    PARAGRAPH_COMPLETION_READ_SUCCESS(HttpStatus.OK, "문단 완성 글 조회 성공");
+    PARAGRAPH_COMPLETION_READ_SUCCESS(HttpStatus.OK, "문단 완성 글 조회 성공"),
+    PARAGRAPH_COMPLETION_READ_ALL_SUCCESS(HttpStatus.OK, "문단 완성 글 전체 조회 성공");
 
     private final HttpStatus status;
     private final String message;
