5ab84a690a537fc75a3852a056a3413056f9d213
한상희 <tkdgml822@gmail.com>
2025-08-28 01:55:17 +0900

    feat: 피드백 생성 에러 추가

---
+++ src/main/java/com/icando/feedback/exception/FeedbackErrorCode.java
@@ -0,0 +1,26 @@
+package com.icando.feedback.exception;
+
+import com.icando.global.error.core.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+public enum FeedbackErrorCode implements ErrorCode {
+
+    FEEDBACK_GENERATION_FAILED(HttpStatus.NOT_FOUND, "피드백 생성을 실패했습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+
+    @Override
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+
+}
