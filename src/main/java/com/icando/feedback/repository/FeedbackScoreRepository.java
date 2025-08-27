980d51b5f6f8f219f2ccfc8f6c12571027bb27e1
한상희 <tkdgml822@gmail.com>
2025-08-28 00:59:26 +0900

    feat: 피드백 점수 레포지스토리 구현

---
+++ src/main/java/com/icando/feedback/repository/FeedbackScoreRepository.java
@@ -0,0 +1,7 @@
+package com.icando.feedback.repository;
+
+import com.icando.feedback.entity.FeedbackScore;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedbackScoreRepository extends JpaRepository<FeedbackScore, Long> {
+}
