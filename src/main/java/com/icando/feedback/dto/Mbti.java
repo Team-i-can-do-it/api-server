8e70f9a75db1b93b664149ff99e38fde6a508521
한상희 <tkdgml822@gmail.com>
2025-08-28 01:45:35 +0900

    feat: DTO 추가

---
+++ src/main/java/com/icando/feedback/dto/Mbti.java
@@ -0,0 +1,13 @@
+package com.icando.feedback.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+// MBTI 점수
+public record Mbti(
+    @JsonProperty("expression_style")
+    Integer expressionStyle,
+    @JsonProperty("content_format")
+    Integer contentFormat,
+    @JsonProperty("tone_of_voice")
+    Integer toneOfVoice
+) {}
