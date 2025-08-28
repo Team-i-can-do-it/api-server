2b4d0995cce9f0c6e86561fc8012ad549da3f8b0
한상희 <tkdgml822@gmail.com>
2025-08-28 11:45:37 +0900

    feat: swagger 설정

--- src/main/java/com/icando/feedback/controller/FeedbackController.java
+++ src/main/java/com/icando/feedback/controller/FeedbackController.java
@@ -5,6 +5,7 @@ import com.icando.feedback.dto.FeedbackResponse;
 import com.icando.feedback.enums.FeedbackSuccessCode;
 import com.icando.feedback.service.FeedbackService;
 import com.icando.global.success.SuccessResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +21,10 @@ public class FeedbackController {
 
     private final FeedbackService feedbackService;
 
+    @Operation(
+        summary = "AI 피드백 받기",
+        description = "피드백 생성 및 저장"
+    )
     @PostMapping
     public ResponseEntity<SuccessResponse<FeedbackResponse>> generateFeedback(@Valid @RequestBody FeedbackRequest reqeust) {
         FeedbackResponse feedbackResponse = feedbackService.generateFeedback(reqeust);
