80413a9854259a8da9eb0379000dcaa15e63a1a0
paka <wjd1357912@gmail.com>
2025-08-31 17:13:57 +0900

    refactor : Dto 필드값 수정 및 image 업로드 로직 추가

--- src/main/java/com/icando/ItemShop/dto/CreateItemRequest.java
+++ src/main/java/com/icando/ItemShop/dto/CreateItemRequest.java
@@ -1,12 +1,11 @@
-package com.icando.randomBox.dto;
+package com.icando.ItemShop.dto;
 
-import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @Setter
@@ -14,12 +13,15 @@ import lombok.Setter;
 public class CreateItemRequest {
 
     @NotBlank(message = "상품명은 필수 입니다")
-    private String item;
+    private String name;
+
+    @NotBlank(message = "상품 이미지는 필수입니다")
+    private MultipartFile imageUrl;
 
     @Min(value = 1, message = "상품의 개수는 최소 1개 이상이어야 합니다")
     private int quantity;
 
-    @Positive(message = "확률은 0보다 커야 됩니다")
-    @DecimalMax(value = "100.0", message = "확률은 100% 이하여야 합니다")
-    private float probability;
+    @Min(value = 10, message = "상품 포인트는 최소 10포인트 이상이어야 합니다")
+    private int point;
+
 }
--- src/main/java/com/icando/ItemShop/service/AdminPointShopService.java
+++ src/main/java/com/icando/ItemShop/service/AdminPointShopService.java
@@ -1,5 +1,6 @@
 package com.icando.ItemShop.service;
 
+import com.icando.global.upload.S3Uploader;
 import com.icando.member.exception.MemberErrorCode;
 import com.icando.member.exception.MemberException;
 import com.icando.member.repository.MemberRepository;
@@ -10,15 +11,16 @@ import com.icando.ItemShop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class ItemService {
+public class AdminPointShopService {
 
     private final ItemRepository randomBoxRepository;
     private final MemberRepository memberRepository;
-    private final PointRepository pointRepository;
+    private final S3Uploader s3Uploader;
 
     //TODO: 추후 관리자 계정 생성 기능 구현 후 admin 계정인지 아닌지에 대한 인증 로직 추가 예정
     @Transactional
@@ -27,22 +29,12 @@ public class ItemService {
         memberRepository.findById(memberId)
                 .orElseThrow(()-> new MemberException(MemberErrorCode.INVALID_MEMBER_ID));
 
-        Item item = Item.of(itemRequest.getItem(), itemRequest.getQuantity(), itemRequest.getProbability());
+        String imageUrl = s3Uploader.upload(itemRequest.getImageUrl(),"item");
+
+        Item item = Item.of(itemRequest.getName(), imageUrl, itemRequest.getQuantity(),itemRequest.getPoint());
 
         randomBoxRepository.save(item);
 
         return item;
     }
-
-//    public RandomBox drawRandomBoxByMemberPoint(Long memberId) {
-//
-//        memberRepository.findById(memberId).
-//                orElseThrow(() -> new MemberException(MemberErrorCode.INVALID_MEMBER_ID));
-//
-//        Point point = pointRepository.findByMemberId(memberId)
-//                .orElseThrow(() -> new MemberException(MemberErrorCode.INVALID_POINT));
-//
-//        //TODO: 추후 갓챠 포인트 결정 후 감소 포인트 조정 예정
-//        point.decreasePoint(10);
-//    }
 }
