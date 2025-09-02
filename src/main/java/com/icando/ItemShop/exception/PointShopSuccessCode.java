6a50ecc8131b0d9d3a89750b8000a10160c3ad8a
paka <wjd1357912@gmail.com>
2025-09-03 01:12:53 +0900

    feat : 응답 처리 메서드 작성

--- src/main/java/com/icando/ItemShop/exception/PointShopSuccessCode.java
+++ src/main/java/com/icando/ItemShop/exception/PointShopSuccessCode.java
@@ -10,7 +10,8 @@ public enum PointShopSuccessCode implements SuccessCode {
     SUCCESS_DRAW_RANDOM_BOX(HttpStatus.OK,"뽑기에 성공하셨습니다" ),
     SUCCESS_CREATE_ITEM(HttpStatus.OK,"상품이 성공적으로 등록되었습니다" ),
     SUCCESS_GET_ITEM_LIST(HttpStatus.OK,"상품 목록을 성공적으로 조회했습니다" ),
-    SUCCESS_GET_ITEM(HttpStatus.OK,"상품 정보를 성공적으로 조회했습니다" );
+    SUCCESS_GET_ITEM(HttpStatus.OK,"상품 정보를 성공적으로 조회했습니다" ),
+    SUCCESS_DELETE_ITEM(HttpStatus.OK,"상품을 성공적으로 삭제했습니다" );
 
     private final HttpStatus status;
     private final String message;
