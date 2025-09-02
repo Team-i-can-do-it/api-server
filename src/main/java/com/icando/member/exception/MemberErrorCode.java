e8afe4f419532d4dc8e529def422b2cf1a81a158
paka <wjd1357912@gmail.com>
2025-09-03 00:37:01 +0900

    feat: 사용자 정의 응답 메서드 작성

--- src/main/java/com/icando/member/exception/MemberErrorCode.java
+++ src/main/java/com/icando/member/exception/MemberErrorCode.java
@@ -8,8 +8,10 @@ import org.springframework.http.HttpStatus;
 public enum MemberErrorCode implements ErrorCode {
 
     INVALID_MEMBER_ID(HttpStatus.BAD_REQUEST, "입력하신 회원 ID가 올바르지 않거나 존재하지 않습니다."),
+    INVALID_MEMBER_EMAIL(HttpStatus.BAD_REQUEST, "회원의 이메일이 올바르지 않거나 존재하지 않습니다."),
     INVALID_POINT(HttpStatus.BAD_REQUEST, "포인트가 존재하지 않습니다" ),
-    NOT_ENOUGH_POINTS(HttpStatus.BAD_REQUEST,"포인트가 부족합니다" );
+    NOT_ENOUGH_POINTS(HttpStatus.BAD_REQUEST,"포인트가 부족합니다" ),
+    NOT_ADMIN_MEMBER(HttpStatus.BAD_REQUEST,"관리자 계정이 아닙니다" );
 
 
     private final HttpStatus status;
