06e5f83b51211ed8d06537a463897dc4f7f27582
한상희 <tkdgml822@gmail.com>
2025-09-02 10:55:33 +0900

    feat: FeedbackReuest DTO 변경

--- src/main/java/com/icando/feedback/dto/FeedbackRequest.java
+++ src/main/java/com/icando/feedback/dto/FeedbackRequest.java
@@ -1,10 +1,13 @@
 package com.icando.feedback.dto;
 
+
+import com.icando.writing.enums.WritingType;
 import jakarta.validation.constraints.NotBlank;
 
 public record FeedbackRequest(
-    @NotBlank(message = "피드백할 내용은 필수입니다.")
-    String content,
-    @NotBlank(message = "글의 주제는 필수입니다.")
-    String topic
+    @NotBlank(message = "타입 정의는 필수입니다.")
+    WritingType writingType,
+
+    @NotBlank(message = "글쓰기Id는 필수입니다.")
+    Long wiringId
 ) {}
