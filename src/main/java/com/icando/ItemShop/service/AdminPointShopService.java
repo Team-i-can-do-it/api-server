6fae52c6004f83c0e4eff129a61f068cb69ccafb
paka <wjd1357912@gmail.com>
2025-09-03 01:13:41 +0900

    feat : 상품 삭제 서비스 로직 작성 및 검증 메서드 작성

--- src/main/java/com/icando/ItemShop/service/AdminPointShopService.java
+++ src/main/java/com/icando/ItemShop/service/AdminPointShopService.java
@@ -1,6 +1,10 @@
 package com.icando.ItemShop.service;
 
+import com.icando.ItemShop.exception.PointShopErrorCode;
+import com.icando.ItemShop.exception.PointShopException;
 import com.icando.global.upload.S3Uploader;
+import com.icando.member.entity.Member;
+import com.icando.member.entity.Role;
 import com.icando.member.exception.MemberErrorCode;
 import com.icando.member.exception.MemberException;
 import com.icando.member.repository.MemberRepository;
@@ -9,6 +13,7 @@ import com.icando.ItemShop.dto.CreateItemRequest;
 import com.icando.ItemShop.entity.Item;
 import com.icando.ItemShop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,6 +26,7 @@ public class AdminPointShopService {
     private final ItemRepository randomBoxRepository;
     private final MemberRepository memberRepository;
     private final S3Uploader s3Uploader;
+    private final ItemRepository itemRepository;
 
     //TODO: 추후 관리자 계정 생성 기능 구현 후 admin 계정인지 아닌지에 대한 인증 로직 추가 예정
     @Transactional
@@ -36,5 +42,26 @@ public class AdminPointShopService {
         randomBoxRepository.save(item);
 
         return item;
+    }
+
+    //TODO: 반복 예외 사항 validate 메서드로 통합 예정
+    public void deleteItemByAdminId(Long itemId, UserDetails userDetails) {
+        validateAdmin(userDetails.getUsername());
+        Item item = validateItem(itemId);
+
+        itemRepository.delete(item);
+    }
+
+    private Member validateAdmin(String email) {
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new MemberException(MemberErrorCode.INVALID_MEMBER_EMAIL));
+
+        if(member.getRole().equals(Role.ADMIN)){ return member; }
+        throw new MemberException(MemberErrorCode.NOT_ADMIN_MEMBER);
+    }
+
+    private Item validateItem(Long itemId) {
+        return itemRepository.findById(itemId)
+                .orElseThrow(() -> new PointShopException(PointShopErrorCode.INVALID_ITEM_ID));
     }
 }
