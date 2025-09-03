64a61ea35d788485af00fd012de021917a2aa2a7
한상희 <tkdgml822@gmail.com>
2025-09-03 10:06:59 +0900

    feat: MBTI 레포지스토리 생성

---
+++ src/main/java/com/icando/member/repository/MbtiRepository.java
@@ -0,0 +1,7 @@
+package com.icando.member.repository;
+
+import com.icando.member.entity.Mbti;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MbtiRepository extends JpaRepository<Mbti, Long> {
+}
