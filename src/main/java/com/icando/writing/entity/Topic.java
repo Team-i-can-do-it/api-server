5c7384ec31da44b99f06615774e85ae43ceaab52
Huey S. Jeong <wjdgnltkd123@gmail.com>
2025-09-03 10:08:37 +0900

    feat: Topic 엔티티에 ReferenceMaterial 관계 추가 및 쿼리 수정

--- src/main/java/com/icando/writing/entity/Topic.java
+++ src/main/java/com/icando/writing/entity/Topic.java
@@ -24,7 +24,10 @@ public class Topic {
     @Column(name = "topic")
     private String topic;
 
-    private Topic(Category category, String topic) {
+    @OneToMany(mappedBy = "topic", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<ReferenceMaterial> referenceMaterials = new ArrayList<>();
+
+    public Topic(Category category, String topic) {
         this.category = category;
         this.topic = topic;
     }
