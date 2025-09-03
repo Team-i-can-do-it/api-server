1dc3ef40f7b641e50e7cae335997de124a947308
한상희 <tkdgml822@gmail.com>
2025-09-03 15:25:07 +0900

    feat: MBTI 컨트롤러 구현

--- src/main/java/com/icando/member/controller/MemberController.java
+++ src/main/java/com/icando/member/controller/MemberController.java
@@ -1,6 +1,14 @@
 package com.icando.member.controller;
 
+import com.icando.global.success.SuccessCode;
+import com.icando.global.success.SuccessResponse;
+import com.icando.member.dto.MbtiRequest;
+import com.icando.member.exception.MemberSuccessCode;
+import com.icando.member.service.MbtiService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
+
+    MbtiService mbtiService;
+
+    @PostMapping("/mbti")
+    public ResponseEntity<SuccessResponse> saveMbti(@Valid MbtiRequest mbtiRequest) {
+        mbtiService.saveMbti(mbtiRequest);
+
+        return ResponseEntity.ok(
+            SuccessResponse.of(MemberSuccessCode.MBTI_SUCCESS_SAVE)
+        );
+
+    }
 }
 
