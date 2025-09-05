778cc84801c4251c87be5c0d9fa0bcffa498cf0a
GARDEN\wjd13 <wjd1357912@gmail.com>
2025-09-05 15:06:35 +0900

    fix : 포인트 증가 오류 서비스 로직 작성

--- src/main/java/com/icando/member/repository/PointHistoryRepository.java
+++ src/main/java/com/icando/member/repository/PointHistoryRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface PointHistoryRepository extends JpaRepository<PointHistory,Long> {
     int countByMemberIdAndCreatedAt(Long memberId, LocalDate today);
+
+    List<PointHistory> findByMemberIdAndCreatedAtBetween(Long memberId, LocalDateTime startOfDay, LocalDateTime endOfDay);
 }
