2dcd6a86b34a9f9ebba974768e7245ea0b49a5af
Huey S. Jeong <wjdgnltkd123@gmail.com>
2025-09-07 01:26:09 +0900

    test: 서비스 테스트 추가 (해피 케이스)

--- src/main/java/com/icando/bookmark/dto/BookmarkAddRequest.java
+++ src/main/java/com/icando/bookmark/dto/BookmarkAddRequest.java
@@ -1,8 +1,10 @@
 package com.icando.bookmark.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class BookmarkAddRequest {
     private long referenceMaterialId;
 }
