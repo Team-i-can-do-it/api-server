69af5af999141362dc7bf423d015a8f49728a068
한상희 <tkdgml822@gmail.com>
2025-09-07 20:51:17 +0900

    test: Mbti 저장 테스트 코드 수정

--- src/test/java/com/icando/member/mbti/MbtiServiceTest.java
+++ src/test/java/com/icando/member/mbti/MbtiServiceTest.java
@@ -9,11 +9,11 @@ import com.icando.member.exception.MemberException;
 import com.icando.member.repository.MbtiRepository;
 import com.icando.member.repository.MemberRepository;
 import com.icando.member.service.MbtiService;
-import org.aspectj.weaver.patterns.BindingAnnotationFieldTypePattern;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -23,6 +23,7 @@ import java.util.Optional;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -38,7 +39,6 @@ public class MbtiServiceTest {
     private MbtiService mbtiService;
 
     private Member member;
-    private Mbti mbti;
     private MbtiRequest mbtiRequest;
 
     @BeforeEach
@@ -51,22 +51,29 @@ public class MbtiServiceTest {
             false
         );
 
-        mbti = Mbti.of("무슨 푸들", "푸들임", "dog.png");
-        mbtiRequest = new MbtiRequest("푸들", "푸들임 ", "intp.png");
+        mbtiRequest = new MbtiRequest("ISTP", "ISTP ", "ISTP.png");
     }
 
     @Test
     @DisplayName("MBTI 캐릭터 저장 성공")
     public void mbtiSave_success() {
         // given
+        MbtiRequest mbtiRequest = new MbtiRequest("ISTP", "조용한 장인", "istp.png");
+
         when(memberRepository.findByEmail(member.getEmail())).thenReturn(Optional.of(member));
-        when(mbtiRepository.findByName(mbtiRequest.name())).thenReturn(Optional.of(mbti));
 
         // when
         mbtiService.saveMbti(mbtiRequest, member.getEmail());
 
         // then
-        assertThat(member.getMbti()).isEqualTo(mbti);
+        ArgumentCaptor<Mbti> mbtiCaptor = ArgumentCaptor.forClass(Mbti.class);
+        verify(mbtiRepository).save(mbtiCaptor.capture());
+        Mbti savedMbti = mbtiCaptor.getValue();
+
+        assertThat(savedMbti.getMember()).isEqualTo(member);
+        assertThat(savedMbti.getName()).isEqualTo(mbtiRequest.name());
+        assertThat(savedMbti.getDescription()).isEqualTo(mbtiRequest.description());
+        assertThat(savedMbti.getImageUrl()).isEqualTo(mbtiRequest.imageUrl());
     }
 
     @Test
