eeb88012b736b5cb5a9b25945d47b7c52a90c75b
한상희 <tkdgml822@gmail.com>
2025-09-08 16:57:00 +0900

    docs: FeedbackController swagger Tag달기

--- src/main/java/com/icando/feedback/controller/FeedbackController.java
+++ src/main/java/com/icando/feedback/controller/FeedbackController.java
@@ -7,6 +7,7 @@ import com.icando.feedback.service.FeedbackService;
 import com.icando.global.success.SuccessResponse;
 import com.icando.member.entity.ActivityType;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,10 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/feedback")
 @RequiredArgsConstructor
+@Tag(
+    name = "AI 피드백",
+    description = "AI를 통한 피드백을 받을 수 있음"
+)
 public class FeedbackController {
 
     private final FeedbackService feedbackService;
