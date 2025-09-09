5c16871e85ed25606474e4fee2440b1c4b043d41
Huey S. Jeong <wjdgnltkd123@gmail.com>
2025-09-09 14:24:38 +0900

    feat: 피드백 생성 기능 개선 및 오류 처리 추가

--- src/main/java/com/icando/feedback/service/FeedbackService.java
+++ src/main/java/com/icando/feedback/service/FeedbackService.java
@@ -12,14 +12,20 @@ import com.icando.feedback.repository.FeedbackScoreRepository;
 import com.icando.member.entity.ActivityType;
 import com.icando.member.service.PointService;
 import com.icando.member.repository.MbtiRepository;
+import com.icando.paragraphCompletion.entity.ParagraphCompletion;
+import com.icando.paragraphCompletion.entity.ParagraphWord;
+import com.icando.paragraphCompletion.repository.ParagraphCompletionRepository;
 import com.icando.writing.entity.Topic;
 import com.icando.writing.entity.Writing;
+import com.icando.writing.enums.WritingType;
 import com.icando.writing.service.WritingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -32,14 +38,47 @@ public class FeedbackService {
     private final WritingService writingService;
     private final MbtiRepository mbtiRepository;
     private final PointService pointService;
+    private final ParagraphCompletionRepository paragraphCompletionRepository;
 
     @Value("${feedback.evaluation.prompt.feedback}")
     private String evaluationPromptFeedback;
 
     // TODO: 추후 stream, Flux 비동기로 성능개선
     @Transactional
-    public FeedbackResponse generateFeedback(FeedbackRequest reqeust, ActivityType activityType) {
-        Writing writing = writingService.getWriting(reqeust.writingId());
+    public FeedbackResponse generateFeedback(FeedbackRequest request, ActivityType activityType) {
+        if (request.writingType() == WritingType.WRITING) {
+            return generateWritingFeedback(request, activityType);
+        } else {
+            return generateParagraphCompletionFeedback(request, activityType);
+        }
+    }
+
+    private FeedbackResponse generateParagraphCompletionFeedback(FeedbackRequest request, ActivityType activityType) {
+        ParagraphCompletion paragraphCompletion = paragraphCompletionRepository.findById(request.writingId())
+                .orElseThrow(() -> new FeedbackException(FeedbackErrorCode.WRITING_NOT_FOUND));
+        String topic = paragraphCompletion.getParagraphWords().stream().map(ParagraphWord::getWord).collect(Collectors.joining(", "));
+
+        // AI에 요청후 JSON 응답을 DTO로 변환
+        FeedbackResponse aiResponse = chatClientBuilder.build()
+                .prompt("다음은 유저가 선택한 단어 목록입니다. " + topic)
+                .system(evaluationPromptFeedback)
+                .user(paragraphCompletion.getContent())
+                .call()
+                .entity(FeedbackResponse.class);
+
+        if (aiResponse == null) {
+            throw new FeedbackException(FeedbackErrorCode.FEEDBACK_GENERATION_FAILED);
+        }
+
+        pointService.earnPoints(paragraphCompletion.getMember().getId(),100,activityType);
+        Feedback savedFeedback = saveFeedback(aiResponse);
+        paragraphCompletion.updateFeedback(savedFeedback);
+        saveFeedbackScore(aiResponse, savedFeedback);
+        return aiResponse;
+    }
+
+    private FeedbackResponse generateWritingFeedback(FeedbackRequest request, ActivityType activityType) {
+        Writing writing = writingService.getWriting(request.writingId());
         Topic topic = writing.getTopic();
 
         // AI에 요청후 JSON 응답을 DTO로 변환
