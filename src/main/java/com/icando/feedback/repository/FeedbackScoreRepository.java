bc3bbcf7c5a2568fd31d9ae537283d870b7e6c3a
Huey S. Jeong <wjdgnltkd123@gmail.com>
2025-09-10 14:22:16 +0900

    refactor: FeedbackScoreRepository에서 불필요한 import 제거

--- src/main/java/com/icando/feedback/repository/FeedbackScoreRepository.java
+++ src/main/java/com/icando/feedback/repository/FeedbackScoreRepository.java
@@ -1,11 +1,7 @@
 package com.icando.feedback.repository;
 
 import com.icando.feedback.entity.FeedbackScore;
-import com.icando.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.time.LocalDate;
-import java.util.SortedMap;
 
 public interface FeedbackScoreRepository extends JpaRepository<FeedbackScore, Long>, FeedbackScoreRepositoryCustom {
 }
