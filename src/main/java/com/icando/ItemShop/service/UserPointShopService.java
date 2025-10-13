6b77cf77824cc83a0aedbb30e6a23bb74806c7b7
paka <wjd1357912@gmail.com>
2025-10-13 18:04:18 +0900

    refactor: 회원 중복 조회 로직 삭제

--- src/main/java/com/icando/ItemShop/service/UserPointShopService.java
+++ src/main/java/com/icando/ItemShop/service/UserPointShopService.java
@@ -184,7 +184,7 @@ public class UserPointShopService {
         Item item = validateItem(itemId);
         Member member = validatePoint(email, item.getPoint());
         PointShopHistory pointShopHistory = PointShopHistory.byPhoneNumber(member,item,number);
-        pointService.usedPoint(member.getId(),item.getPoint(), ActivityType.BUY);
+        pointService.usedPoint(member,item.getPoint(), ActivityType.BUY);
         item.decreaseQuantity(1);
         pointShopHistoryRepository.save(pointShopHistory);
         return item;
